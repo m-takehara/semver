4980a4e85b2d668111673b6589ce49659d5e235f
m-takehara <m.takehara.0611@gmail.com>
2019-07-19 00:39:42 +0900

    refactor: テストケースの説明文を修正

--- tests/version-number.test.ts
+++ tests/version-number.test.ts
@@ -3,7 +3,7 @@ import {VersionNumber} from "../src/version-number";
 describe('VersionNumber クラスのテスト', () => {
     describe('バージョン番号クラスは自身の文字列表現を返すメソッドを持つ', () => {
         describe('文字列表現は major, minor, patch の順に . で連結されたものである', () => {
-            test('コンストラクタに (0, 0, 0) を与えると文字列 "0.0.0" が得られる', () => {
+            test('major = 0, minor = 0, patch = 0 のとき文字列 "0.0.0" が得られる', () => {
                 const number = new VersionNumber(0, 0, 0);
                 expect(number.stringify()).toBe('0.0.0');
             });

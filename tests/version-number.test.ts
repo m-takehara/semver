6616ddee1a55847e498cb17dad78c155ecedc37c
m-takehara <m.takehara.0611@gmail.com>
2019-07-19 00:45:37 +0900

    test: equals の異常系テストを作成(RED→GREEN)

--- tests/version-number.test.ts
+++ tests/version-number.test.ts
@@ -18,5 +18,13 @@ describe('VersionNumber クラスのテスト', () => {
                 expect(numberA.equals(numberB)).toBeTruthy();
             });
         });
+
+        describe('いずれかのフィールドの値が比較対象と異なる場合は false を返す', () => {
+            test('major の値が 0 のオブジェクトと 1 のオブジェクトを比較すると false が得られる', () => {
+                const numberA = new VersionNumber(1, 0, 0);
+                const numberB = new VersionNumber(0, 0, 0);
+                expect(numberA.equals(numberB)).toBeFalsy();
+            });
+        });
     });
 });

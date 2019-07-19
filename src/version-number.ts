96c6214500ea81fd81802446ef362edefd1b1902
m-takehara <m.takehara.0611@gmail.com>
2019-07-20 00:39:30 +0900

    fix: 大小比較用メソッドの実装を変更(GREEN)

--- src/version-number.ts
+++ src/version-number.ts
@@ -15,7 +15,17 @@ export class VersionNumber {
             && this.patch === comparison.patch;
     }
 
-    public compareTo(comparison: VersionNumber): number {
+    public compareTo(comparison: VersionNumber): -1 | 0 | 1 {
+        if (this.major !== comparison.major) {
+            return this.major < comparison.major ? -1 : 1;
+        }
+        if (this.minor !== comparison.minor) {
+            return this.minor < comparison.minor ? -1 : 1;
+        }
+        if (this.patch !== comparison.patch) {
+            return this.patch < comparison.patch ? -1 : 1;
+        }
         return 0;
     }
+
 }
